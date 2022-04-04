dd349701884f3583a4cf4cee84b3c67a51ee04a1
anthgrim <anthgrim@icloud.com>
2022-04-03 23:50:07 -0400

    Header animated background

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
-//Resume Download
+let particlesArr = [];
+let header = document.getElementById("header");
 
 //Quotes
 const quoteBtn = document.getElementById("quoteBtn");
@@ -27,3 +28,32 @@ const alertQuote = async () => {
 };
 
 quoteBtn.addEventListener("click", alertQuote);
+
+//Animated background
+const generateParticles = () => {
+  let newParticle = document.createElement("div");
+  newParticle.classList.add("particle");
+  newParticle.style.left = `${Math.random() * header.clientWidth}px`;
+  newParticle.style.top = `${Math.random() * header.clientHeight}px`;
+  header.appendChild(newParticle);
+  particlesArr.push(newParticle);
+};
+
+const resetParticles = () => {
+  if (particlesArr.length === 0) return;
+  for (let i = 0; i < particlesArr.length; i++) {
+    particlesArr[i].remove();
+  }
+  particlesArr = [];
+};
+
+const animateParticles = () => {
+  for (let i = 0; i < 100; i++) {
+    generateParticles();
+  }
+};
+
+setInterval(() => {
+  resetParticles();
+  animateParticles();
+}, 100);
